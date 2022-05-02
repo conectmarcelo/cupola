24e4a5dff156ae8b4b1035bc6b06b445d71ee4df
conectmarcelo <conectmarcelo@hotmail.com>
2022-05-02 04:50:23 -0300

    teste

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="teste">
+        <div class="prototype">
+          <div class="conectOverviweCard" v-for="(dado, i) in dados" :key="i">
+            <div v-show="active" class="conectaTooltip">
+              <p class="hint">{{dado.legenda}}</p>
+              <div class="arrow"></div>
+            </div>
+            <div class="conectaIcon">
+              <v-icon
+                class="info"
+                @mouseover="mouseOver"
+                @mouseleave="mouseLeave"
+              >mdi-information-outline</v-icon>
+            </div>
+            <div class="card">
+              <v-row class="wrap">
+                <div class="symbol">
+                  <v-avatar class="circle" :color="dado.cor" size="55.37" />
+                  <v-icon id="icon" :color="dado.cor">{{dado.icone}}</v-icon>
+                </div>
+                <div class="content">
+                  <P class="value">{{dado.valor}}</P>
+                  <p class="label">{{ dado.nome }}</p>
+                </div>
+              </v-row>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+
+  data() {
+    return {
+      active: false,
+
+      dados: [
+        {
+          nome: "Coversões",
+          valor: "2.5 M",
+          icone: "mdi-email",
+          cor: "blue",
+          legenda: "Conversão é a ação do envio de qualquer formulário.",
+        },
+        {
+          nome: "Leads",
+          valor: "965.321",
+          icone: "mdi-account",
+          cor: "purple",
+          legenda:
+            "Lead é um contato individual do cliente ao enviar formulários.",
+        },
+        {
+          nome: "Cust por lead",
+          valor: "R$ 10",
+          icone: "mdi-currency-usd",
+          cor: "green",
+          legenda:
+            "É o cálculo do investimento utilizado pela quantidade de leads gerados no período.",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    mouseOver: function () {
+      this.active = true;
+    },
+    mouseLeave: function () {
+      this.active = false;
+    },
+  },
+};
+</script>
+
+<style src="./style.scss" lang='scss' scoped/>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                blue: '#1E93FF',
+                purple: '#B030FF',
+                green: '#28CC56',
+
+
+
+            },
+        },
+    },
+})
